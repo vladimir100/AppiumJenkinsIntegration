30fa99abc6353c8d1ab2508a94676ced95936742
Vladimir Belorusets <vladimir100@hotmail.com>
2016-02-20 20:53:31 -0800

    My first commit

---
+++ src/main/java/com/appium/android/App.java
@@ -0,0 +1,13 @@
+package com.appium.android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
